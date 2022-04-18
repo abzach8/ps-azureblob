6e24ca09222b7bcd96703433698396a61a72fd03
Abby <58935958+abzach8@users.noreply.github.com>
2022-04-18 18:19:47 +0530

    Add files via upload
    PowerShell script to upload file to Azure blob storage. You need to modify the URL according to your environment and configuration

---
+++ upload-to-azure-blob.ps1
@@ -0,0 +1,23 @@
+﻿# Change execution policy
+# Set-ExecutionPolicy Unrestricted -Scope CurrentUser
+
+# Start logging
+$logfilepath="C:\temp\upload-to-azureblob-script.log"
+Start-Transcript -Path $logfilepath
+
+# Specify file to upload
+$file = "C:\temp\sample.txt"
+$name = (Get-Item $file).Name
+
+# Target URL format = SAS-URL/Container/Folder/$name?SAS-token
+$uri = "https://abzachstorageaccount.blob.core.windows.net/sample-container/$($name)?{your-SAS-token}"
+# Required headers
+$headers = @{
+    'x-ms-blob-type' = 'BlockBlob'
+}
+
+# Uploading file
+Invoke-RestMethod -Uri $uri -Method Put -Headers $headers -InFile $file
+
+# Stop logging
+Stop-Transcript
