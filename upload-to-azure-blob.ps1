6c2ef7a9c5e52d5d8a30a978cc720825c09bd820
Abbia Zacharia <58935958+abzach8@users.noreply.github.com>
2022-11-15 16:34:31 +0530

    Update upload-to-azure-blob.ps1

--- upload-to-azure-blob.ps1
+++ upload-to-azure-blob.ps1
@@ -1,23 +1,61 @@
-﻿# Change execution policy
-# Set-ExecutionPolicy Unrestricted -Scope CurrentUser
+<#
 
-# Start logging
-$logfilepath="C:\temp\upload-to-azureblob-script.log"
-Start-Transcript -Path $logfilepath
+Name: Upload-To-Storage-Container.ps1
+Version: 2
+Author: Abbia Zacharia
 
-# Specify file to upload
-$file = "C:\temp\sample.txt"
+Purpose: 
+Upload local files to Azure storage container
+
+Description:
+
+Version Control:
+
+Prerequisites:
+
+Configurations:
+
+#>
+
+#==================================================================================
+
+function StorageUpload()
+{
+
+# Select file
+Write-Host "Example: C:\temp\sample.txt" -ForegroundColor Blue
+$file = Read-Host -Prompt "Enter full path for file to be uploaded"
 $name = (Get-Item $file).Name
-
-# Target URL format = SAS-URL/Container/Folder/$name?SAS-token
-$uri = "https://abzachstorageaccount.blob.core.windows.net/sample-container/$($name)?{your-SAS-token}"
-# Required headers
+$uri = "https://$storagename.blob.core.windows.net/$containername/$($name)?$unsecuretoken"
 $headers = @{
     'x-ms-blob-type' = 'BlockBlob'
 }
 
-# Uploading file
-Invoke-RestMethod -Uri $uri -Method Put -Headers $headers -InFile $file
+# Upload file
+Write-Host "Upload started" -ForegroundColor Yellow
+Invoke-RestMethod -Uri $uri -Method Put -Headers $headers -InFile $file -ErrorAction Stop
+Write-Host "$name uploaded to $containername" -ForegroundColor Green
+}
 
-# Stop logging
-Stop-Transcript
+# Code is executing from here
+Write-Host "`nPowerShell to upload local files to Azure storage container" -ForegroundColor Yellow
+Write-Host "************************" -ForegroundColor White -BackgroundColor Black
+Write-Host "Time to supply AZ storage details" -ForegroundColor Red
+$storagename = Read-Host -Prompt "Enter the name of the storage account"
+$containername = Read-Host -Prompt "Enter the name of the container"
+$sastoken = Read-Host -Prompt "Enter SAS token to access $containername (shared access signature)" -AsSecureString
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($sastoken)
+$unsecuretoken = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+StorageUpload
+$confirmation = Read-Host "Do you want to upload one more file? y/n"
+if ($confirmation -eq 'y') 
+{
+StorageUpload
+}
+else
+{
+exit
+}
+
+#==================================================================================
